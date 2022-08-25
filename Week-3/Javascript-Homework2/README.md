5db4164feedd1aafe53fa32afd6c8dd37a36f215
Zehra Baran <brnzehraa@gmail.com>
2022-08-25 17:25:48 +0300

    Local storage add and delete operations have been edited and added read me file

---
+++ Week-3/Javascript-Homework2/README.md
@@ -0,0 +1,6 @@
+# Create To Do List Using Javascript
+
+## Operations performed:
+- Empty characters cannot be added to the list
+- Functions that perform add, delete and done operations are available
+- Add and delete operations are performed in local storage.
--- Week-3/Javascript-Homework2/todolist.js
+++ Week-3/Javascript-Homework2/todolist.js
@@ -1,3 +1,4 @@
+const defaultValue = ['3 Litre Su İç', 'Ödevleri Yap', 'En Az 3 Saat Kodlama Yap', 'Yemek Yap', '50 Sayfa Kitap Oku']
 //Select ul,li
 let ulList_DOM = document.querySelector("#list")
 let liDOM = document.querySelector("#list-item")
@@ -7,70 +8,63 @@ let formButton = document.querySelector("#liveToastBtn")
 formButton.addEventListener("click", addToDo)
 
 //Select delete button
-let deleteButton = document.querySelectorAll(".icon")
-deleteButton.forEach((element) => {
-  element.addEventListener("click", deleteToDo)
-})
+const addEventDelete = () => {
+  let deleteButton = document.querySelectorAll(".icon")
+  deleteButton.forEach((element) => {
+    element.addEventListener("click", deleteToDo)
+  })
+}
+addEventDelete()
 
 //Select Task Label
-let taskLabel = document.querySelectorAll("#task-label-area")
+let taskLabel = document.querySelectorAll(".list-item")
 taskLabel.forEach((element) => {
   element.addEventListener("click", updateStatus)
 })
 
-localStorage.setItem('todos', JSON.stringify(['3 Litre Su İç', 'Ödevleri Yap', 'En Az 3 Saat Kodlama Yap', 'Yemek Yap', '50 Sayfa Kitap Oku']))
+localStorage.setItem('todos', JSON.stringify(defaultValue))
 
 function addToDo(e) {
   let inputArea = document.querySelector("#task")
   const newTodo = inputArea.value.trim()
 
   if (newTodo === "") {
-    console.log("deger griiniz")
     showAlert("Lütfen bir to do girin")
-    //showToast()
 
   } else {
 
-    //   <li id="list-item">
-    //   <label class="task-label" id="task-label-area">
-    //     <input onclick="updateStatus(0)" type="checkbox" id="1" class="w-auto">
-    //     <span class="pl-2"> 3 Litre Su İç</span>
-    //   </label>
-    //   <i   class="fa fa-times icon"></i>
-    // </li>
-
     //Create List Item
     let liDOMV = document.createElement("li")
-    // liDOM.id ="list-item"
-    // let label = document.createElement("label")
-    // let input = document.createElement("input")
-    // let span = document.createElement("span")
-    // span.innerHTML = `${newTodo}`
+    liDOMV.setAttribute('id', 'list-item')
+    liDOMV.setAttribute('class', 'list-item')
+    liDOMV.addEventListener("click", updateStatus)
 
     liDOMV.innerHTML = `
     <label class="task-label" id="task-label-area">
-      <input onclick="updateStatus(${ulList_DOM.children.length})" type="checkbox" id="${ulList_DOM.children.length}" class="w-auto">
       <span class="pl-2">${newTodo}</span>
     </label>
     <i class="fa fa-times icon"></i>
   `
+
     ulList_DOM.appendChild(liDOMV)
     showAlertSuccess()
     addTodoToStorage(newTodo)
   }
+  addEventDelete()
   e.preventDefault();
 }
 
 function deleteToDo(e) {
+  //debugger
+  console.log(e.target.parentElement)
   e.target.parentElement.remove()
-  deleteTodoFromStorage(e.target.parentElement.textContent)
 
+  deleteTodoFromStorage(e.target.parentElement.textContent)
 }
 
 function updateStatus(e) {
-  if (typeof e === 'number') {
-
-    let taskName = taskLabel[e].lastElementChild
+  if (typeof e !== 'number') {
+    let taskName = e.currentTarget //li emenetine offsetParent ile ulaşılır. Direkt liye checked eklendi.
 
     if (taskName.className.includes("checked")) {
       taskName.classList.remove("checked")
@@ -94,7 +88,6 @@ function addTodoToStorage(newTodo) {
   localStorage.setItem("todos", JSON.stringify(todos))
 }
 
-
 function deleteTodoFromStorage(deletetodo) {
   let todos;
 
@@ -109,7 +102,6 @@ function deleteTodoFromStorage(deletetodo) {
 
 //Select Toast
 let toastArea = document.querySelector("#liveToast")
-toastArea.addEventListener("click", showToast)
 
 function showAlert() {
   let myAlert = document.querySelector('.error');//select id of toast
